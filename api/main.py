cf120cb7f0d9c2804906e5f62bdfd722780c955e
geometrein <contact.khachatryan@gmail.com>
2024-05-18 13:41:38 +0300

    remove auth

--- api/main.py
+++ api/main.py
@@ -1,13 +1,14 @@
 import logging
+import os
 
 from fastapi import FastAPI, Depends, HTTPException
-
-from src.auth import token_validation
+from pydantic import BaseModel
 from src.events import meetup_main
 
 # logging
 LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT, datefmt=DATE_FORMAT)
 logger = logging.getLogger(__name__)
 
@@ -15,16 +16,23 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
+class EventItem(BaseModel):
+    token: str
+
+
 @app.get("/")
 async def root():
     return {"message": "-Luke, I am your API. -Noooo.", "status_code": 200}
 
 
-@app.get("/publish-events/", dependencies=[Depends(token_validation)])
-async def publish_events():
+@app.post("/publish-events/")
+async def publish_events(item: EventItem):
     try:
-        await meetup_main()
-        return {"message": "Successfully published events.", "status_code": 200}
+        if item.token == AUTH_TOKEN:
+            await meetup_main()
+            return {"message": "Successfully published events.", "status_code": 200}
+        else:
+            raise HTTPException(status_code=403,  detail="You shall not pass!")
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Encountered an Unexpected error: {e}"
